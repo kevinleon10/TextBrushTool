933b41f056596843ac78272d090f1420502e27b9
kevinleon10 <lskevin97@hotmail.com>
2024-03-17 16:59:33 -0600

    Making text bold

--- app/src/main/java/com/example/textbrushtool/MainActivity.kt
+++ app/src/main/java/com/example/textbrushtool/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -116,6 +117,7 @@ fun DrawText() {
     }
     val style = TextStyle(
         fontSize = 50.sp,
+        fontWeight = FontWeight.Bold,
         color = Color.White,
     )
 
