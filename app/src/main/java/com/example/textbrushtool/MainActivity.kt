455abb5f2546d68e527f90c635d602105691c5f5
kevinleon10 <lskevin97@hotmail.com>
2024-03-17 14:24:55 -0600

    Adding logic to draw line

--- app/src/main/java/com/example/textbrushtool/MainActivity.kt
+++ app/src/main/java/com/example/textbrushtool/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.textbrushtool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,12 +14,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.textbrushtool.ui.theme.TextBrushToolTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,6 +52,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DrawTool() {
+    var startDrawing by remember { mutableStateOf(false) }
+
     Image(
         painterResource(R.drawable.background),
         contentDescription = "Background",
@@ -52,8 +68,8 @@ fun DrawTool() {
             contentDescription = "Discard",
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .size(Dp(50F))
-                .padding(Dp(10f))
+                .size(50.dp)
+                .padding(10.dp)
         )
     }
 
@@ -63,9 +79,58 @@ fun DrawTool() {
             contentDescription = "Draw",
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .size(Dp(50F))
-                .padding(Dp(10f))
+                .size(50.dp)
+                .padding(10.dp)
+                .clickable {
+                    startDrawing = true
+                }
         )
+    }
+
+    if (startDrawing) {
+        DrawText()
+    }
+}
+
+@Composable
+fun DrawText() {
+    val positions = remember {
+        mutableStateListOf(Pair(Offset(0.0F, 0.0F), Offset(0.0F, 0.0F)))
+    }
+    var currentIndex by remember {
+        mutableIntStateOf(0)
+    }
+    var previousTimeMillis by remember {
+        mutableLongStateOf(0L)
+    }
+
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures { change, _ ->
+                    change.consumeAllChanges()
+                    if (previousTimeMillis != change.previousUptimeMillis) {
+                        positions.add(currentIndex, Pair(change.position, change.position))
+                        previousTimeMillis = change.uptimeMillis
+                    } else {
+                        positions.add(
+                            currentIndex,
+                            Pair(positions[currentIndex].first, change.position)
+                        )
+                        currentIndex++
+                    }
+                }
+            }
+    ) {
+        positions.forEach {
+            drawLine(
+                start = it.first,
+                end = it.second,
+                color = Color.White,
+                strokeWidth = 5.dp.toPx()
+            )
+        }
     }
 }
 
