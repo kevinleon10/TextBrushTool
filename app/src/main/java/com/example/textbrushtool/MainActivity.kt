8636778e96df6dcd643406fc966b88a60c9582d7
kevinleon10 <lskevin97@hotmail.com>
2024-03-17 13:33:57 -0600

    Adding background, close and edit images

--- app/src/main/java/com/example/textbrushtool/MainActivity.kt
+++ app/src/main/java/com/example/textbrushtool/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.textbrushtool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.example.textbrushtool.ui.theme.TextBrushToolTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DrawTool()
                 }
             }
         }
@@ -30,17 +37,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DrawTool() {
+    Image(
+        painterResource(R.drawable.background),
+        contentDescription = "Background",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .fillMaxSize()
     )
+
+    Box(contentAlignment = Alignment.TopStart) {
+        Image(
+            painterResource(R.drawable.close),
+            contentDescription = "Discard",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(Dp(50F))
+                .padding(Dp(10f))
+        )
+    }
+
+    Box(contentAlignment = Alignment.TopEnd) {
+        Image(
+            painterResource(R.drawable.edit),
+            contentDescription = "Draw",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(Dp(50F))
+                .padding(Dp(10f))
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
-fun GreetingPreview() {
+fun DrawToolPreview() {
     TextBrushToolTheme {
-        Greeting("Android")
+        DrawTool()
     }
 }
